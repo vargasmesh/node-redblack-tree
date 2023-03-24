060545715de5e30f98709c94c892674febed8209
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-03-24 18:48:40 -0300

    Feat: Add Traversal functions

---
+++ src/traversal.ts
@@ -0,0 +1,32 @@
+import { Node } from "./RedBlackTree";
+
+type TraversalFunc = (key: number) => void;
+
+const inorder = (node?: Node, f: TraversalFunc = console.log) => {
+  if (node === null) {
+    return;
+  }
+  inorder(node.left, f);
+  f(node.key);
+  inorder(node.right, f);
+};
+
+const postorder = (node?: Node, f: TraversalFunc = console.log) => {
+  if (node === null) {
+    return;
+  }
+  postorder(node.left, f);
+  postorder(node.right, f);
+  f(node.key);
+};
+
+const preorder = (node?: Node, f: TraversalFunc = console.log) => {
+  if (node === null) {
+    return;
+  }
+  f(node.key);
+  preorder(node.left, f);
+  preorder(node.right, f);
+};
+
+export { inorder, postorder, preorder };
